879f7c6dae68b0ff7464d727dd7de8f15494a447
kalpit vyas <kalpitvyas0716@gmail.com>
2022-01-24 09:14:02 +0530

    evenNumber

---
+++ src/main/java/addTwoNumbers/TestApp.java
@@ -0,0 +1,10 @@
+package addTwoNumbers;
+
+public class TestApp {
+	public static void main(String[] args) {
+		System.out.println(isEvenNumber(22));
+	}
+	public static boolean isEvenNumber(int num) {
+		return num%2==0;
+	}
+}
---
+++ src/test/java/addTwoNumbers/TestAppTesting.java
@@ -0,0 +1,12 @@
+package addTwoNumbers;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestAppTesting {
+	@Test
+	public void addition() {
+		assertTrue(TestApp.isEvenNumber(22));
+	}
+}
